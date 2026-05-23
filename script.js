b9478659c88f0eec1f60c40f451ac5bb4da170c1
harytri87 <harytri87@gmail.com>
2026-05-23 11:51:37 +0700

    Display registlet complete list
    Complete list of registlet will be displayed
    when first time opening the page
    (the same style as before application rebuild)

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+function display(...args) {
+  const regisContainer = document.querySelector(".regis-container");
+
+  args.forEach(element => {
+    regisContainer.appendChild(element);
+  });
+}
+
+function convertToHTML(data) {
+  for (const registlet of data) {
+    const nameH2 = document.createElement("h2");
+    const nameA = document.createElement("a");
+    const detail = convertDetail(registlet.detail);
+    const stdLv = document.createElement("p");
+    const maxLvRarity = document.createElement("p");
+    const slug = generateSlug(registlet.name);
+
+    nameA.classList = "heading-anchor";
+    nameA.href = `#${slug}`;
+    nameA.id = slug;
+    nameA.textContent = registlet.name;
+    nameH2.append(nameA);
+
+    stdLv.textContent = `(${registlet.lv.join(", ")})`;
+    maxLvRarity.innerHTML = `Max Level: ${registlet.max_lv}<br>Rarity: ${registlet.rarity}`;
+
+    display(nameH2, stdLv, detail, maxLvRarity);
+  }
+}
+
+function convertDetail(string) {
+  const arrPara = string.split("\r\n> \r\n");
+  const detail = document.createElement("blockquote");
+  const regexBlockquote = />\s/g;
+  const regexNewLine = /\r\n/g;
+
+  for (const detailPara of arrPara) {
+    const para = document.createElement("p");
+    const formatted = detailPara
+      .replace(regexNewLine, "<br/>")
+      .replace(regexBlockquote, "");
+    const codeTagged = formatted
+      .replace(/`([^`]+)`/g, '<code>$1</code>');
+
+    para.innerHTML = codeTagged;
+    detail.appendChild(para);
+  }
+
+  return detail;
+}
+
+function generateSlug(text) {
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '') // hapus karakter khusus
+    .replace(/[\s_-]+/g, '-') // replace spasi/underscore jadi dash
+    .replace(/^-+|-+$/g, ''); // hapus dash di awal/akhir
+}
+
 function filterLevel(data, level) {
   return data.filter((registlet) => registlet.lv.includes(level));
 }
@@ -30,8 +90,5 @@ document.addEventListener("DOMContentLoaded", async (event) => {
   const registletUrl = "./registlet/registlet.json";
   const registletJSON = await fetchRegistletJSON(registletUrl);
 
-  console.log(registletJSON);
-  console.log(filterLevel(registletJSON.registlet, 70));
-  console.log(filterKeyword(registletJSON.registlet, "pror"));
-  console.log(filterKeyword(registletJSON.registlet, "pror", 190));
+  convertToHTML(registletJSON.registlet);
 });
